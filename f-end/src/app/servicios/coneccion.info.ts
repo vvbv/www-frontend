96d5b684ed0f8f713e508667933ee380b7c2f07b
Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
2017-09-23 23:49:25 -0500

    Avance en usuario.service.ts

--- f-end/src/app/servicios/coneccion.info.ts
+++ f-end/src/app/servicios/coneccion.info.ts
@@ -9,6 +9,7 @@ export class ConeccionInfo {
     public url_base: string;
     public url_base_api: string;
     public url_eventos: string;
+    public url_usuarios: string;
     public url_obtener_token: string;
     public url_actualizar_token: string;
     public url_validar_token: string;
@@ -19,11 +20,12 @@ export class ConeccionInfo {
         this.headers = new Headers();
         this.headers.append('Content-Type', 'application/json');
         this.url_base = 'http://127.0.0.1:8000';
-        this.url_base_api = this.url_base + '/api';
+        this.url_base_api = this.url_base + '/api/v1';
         this.url_obtener_token = this.url_base_api + '/auth-jwt/';
         this.url_actualizar_token = this.url_base_api + '/auth-jwt-refresh/';
         this.url_validar_token = this.url_base_api + '/auth-jwt-verify/';
         this.url_eventos = this.url_base_api + '/eventos/';
+        this.url_usuarios = this.url_base_api + '/usuarios/';
     }
     setToken(token: string) {
         this.token = token;
