803085f6c7d1ca69d67637d6f2b04c78fec64b06
Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
2017-11-01 23:22:06 -0500

    Backfront: Aceptar preinscripciones.

--- f-end/src/app/servicios/preInscripcion.service.ts
+++ f-end/src/app/servicios/preInscripcion.service.ts
@@ -11,6 +11,18 @@ import 'rxjs/add/operator/map';
 export class PreInscripcionService{
 
     constructor(private http: Http, private coneccionInfo: ConeccionInfo ){};
+
+    public getPreinscripcion(idPreinscripcion: string): Promise<PreInscripcion>{
+        return this.http
+        .get(this.coneccionInfo.url_preinscripcion + idPreinscripcion, {headers: this.coneccionInfo.headers})
+        .toPromise()
+        .then(
+            response =>  {
+                return (JSON.parse(response.text().toString()) as PreInscripcion);
+            }
+        );
+    }
+
     public registrarPreInscripcion(preInscripcion:PreInscripcion): Promise<JSON|PreInscripcion>{
         return this.http
         .post(this.coneccionInfo.url_preinscripcion, JSON.stringify(preInscripcion), {headers: this.coneccionInfo.headers})
@@ -22,6 +34,34 @@ export class PreInscripcionService{
         ).catch(
             response => {
                 return  (response.text().toString());
+            }
+        );
+    }
+
+    /*
+        Estados posibles:
+        A: Aceptado.
+        R: Rechazado.
+        E: En espera.
+    */
+    public cambiarEstadoPreInscripcion(idPreinscripcion: string, estado: string): Promise<PreInscripcion|JSON>{
+        return this.getPreinscripcion(idPreinscripcion).then(
+            response => {
+                var preInscripcion: PreInscripcion;
+                preInscripcion = response;
+                preInscripcion.estado = estado;
+                return this.http.post(this.coneccionInfo.url_preinscripcion, JSON.stringify(preInscripcion), {headers: this.coneccionInfo.headers})
+                .toPromise()
+                .then(
+                    retorno => {
+                        return JSON.parse(retorno.text().toString()) as PreInscripcion;
+                    }
+                )
+                .catch(
+                    retorno => {
+                        return JSON.parse(retorno.text().toString());
+                    }
+                );
             }
         );
     }
