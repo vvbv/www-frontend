c6dc327e930a048f51194757e51d0231566ae6f0
luchoman08 <luis.manrique@correounivalle.edu.co>
2017-11-17 01:37:57 -0500

    Coneccion a amazon

--- f-end/src/app/servicios/coneccion.info.ts
+++ f-end/src/app/servicios/coneccion.info.ts
@@ -33,7 +33,7 @@ export class ConeccionInfo {
     constructor() {
         this.token_name = 'tok';
         this.token_prefix = 'JWT';
-        this.url_base = 'http://127.0.0.1:8000';
+        this.url_base = 'http://ec2-54-244-74-163.us-west-2.compute.amazonaws.com:8000';
         this.url_base_api = this.url_base + '/api/v1';
         this.url_obtener_token = this.url_base_api + '/auth-jwt/';
         this.url_actualizar_token = this.url_base_api + '/auth-jwt-refresh/';
