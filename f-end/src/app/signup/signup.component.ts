e16fe860b4d8711c47ef2c3f12cc22ff33221c44
luchoman08 <luis.manrique@correounivalle.edu.co>
2017-12-15 02:42:26 -0500

    Calendario De Actividaes en participacion

--- f-end/src/app/signup/signup.component.ts
+++ f-end/src/app/signup/signup.component.ts
@@ -23,6 +23,7 @@ export class SignupComponent implements OnInit {
         this.errorRetorno = JSON.parse('{}');
     }
     crearUsuario(usuario: Usuario) {
+        if(this.checkPwd(usuario.password)){
         this.usuariosService.crearUsuario(usuario)
         .then(response =>  {
             if (this.usuario.nombres === response.nombres) {
@@ -48,6 +49,33 @@ export class SignupComponent implements OnInit {
         }
         )
         .catch(response => { console.log(response); });
+    }
+
+    }
+    keyuppass($event){
+        this.errorRetorno['password'] = '';
+    }
+    checkPwd(str) {
+        if (str.length < 6) {
+            this.errorRetorno['password'] = 'Contraseña demaciado corta';
+            return false;
+        } else if (str.length > 50) {
+            this.errorRetorno['password'] = 'Contraseña demaciado larga';
+            return false;
+        } else if (str.search(/\d/) == -1) {
+            this.errorRetorno['password'] = 'Ingrese almenos un número';
+            return false;
+        } else if (str.search(/[a-z]/) == -1) {
+            this.errorRetorno['password'] = 'Almenos una letra en minuscula';
+            return false;
+        } else if (str.search(/[A-Z]/) == -1) {
+            this.errorRetorno['password'] = 'Almenos una letra en mayuscula';
+            return false;
+        } else if (str.search(/[^a-zA-Z0-9\!\@\#\$\%\^\&\*\(\)\_\+\-]/) != -1) {
+            this.errorRetorno['password'] = 'Ingrese caracterres válidos';
+            return false;
+        }
+        return true;
     }
     ngOnInit() { }
 }
